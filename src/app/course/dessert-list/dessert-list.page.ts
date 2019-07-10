4529823f4bb3e05e1898a739cb175f591d8cf305
Elcio Junior <julioneljunior@gmail.com>
2019-07-10 19:59:43 -0300

    refactor(all files): added return and loading;

--- src/app/course/dessert-list/dessert-list.page.ts
+++ src/app/course/dessert-list/dessert-list.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DessertService } from '../dessert.service';
 import { RestaurantsService } from '../../restaurants/restaurants.service';
 import { TokenService } from '../../guard/token.service';
 import { ActivatedRoute } from '@angular/router';
+import { OverlayService } from '../../common/services/overlay.service';
 
 @Component({
 	selector: 'app-dessert-list',
 	templateUrl: './dessert-list.page.html',
 	styleUrls: ['./dessert-list.page.scss'],
 })
-export class DessertListPage implements OnInit {
+export class DessertListPage {
 	private restaurantId = null;
 	private restaurant = null;
 	private dessertList = [];
@@ -18,9 +19,11 @@ export class DessertListPage implements OnInit {
 		private route: ActivatedRoute,
 		private dessertService: DessertService,
 		private restaurantService: RestaurantsService,
-		private tokenService: TokenService) { }
+		private tokenService: TokenService,
+		private overlayService: OverlayService) { }
 
-	ngOnInit() {
+	async ionViewWillEnter() {
+		let loading = await this.overlayService.loading();
 		this.restaurantId = this.route.snapshot.paramMap.get('restaurantId');
 		this.restaurantService.getRestautant(this.restaurantId)
 			.subscribe(restaurantResponse => {
@@ -32,6 +35,7 @@ export class DessertListPage implements OnInit {
 		this.dessertService.listDesserts(this.restaurantId)
 			.subscribe(dessertListResponse => {
 				this.dessertList = dessertListResponse;
+				loading.dismiss();
 			});
 	}
 

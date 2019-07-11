c80b75c55f3486d889797967e246eca8ffbed1af
Elcio Junior <julioneljunior@gmail.com>
2019-07-11 17:08:12 -0300

    feat(all files): rating WIP + fixed screen;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,86 +3,87 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'sign-in',
-    pathMatch: 'full'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  },
-  {
-    path: 'sign-up',
-    loadChildren: './sign-up/sign-up.module#SignUpPageModule'
-  },
-  {
-    path: 'restaurants',
-    loadChildren: './restaurants/restaurant-list/restaurants.module#RestaurantsPageModule'
-  },
-  {
-    path: 'sign-in',
-    loadChildren: './sign-in/sign-in.module#SignInPageModule'
-  },
-  {
-    path: 'restaurant-form',
-    //canActivate: [AuthGuard],
-    loadChildren: './restaurants/restaurant-form/restaurant-form.module#RestaurantFormPageModule'
-  },
-  {
-    path: 'restaurant/:id',
-    loadChildren: './restaurants/restaurant-view/restaurant-view.module#RestaurantViewPageModule'
-  },
-  {
-    path: 'reservations-list',
-    loadChildren: './reservations/reservations-list/reservations-list.module#ReservationsListPageModule'
-  },
-  {
-    path: 'reservation/:restaurantId/:reservationId',
-    loadChildren: './reservations/reservation-view/reservation-view.module#ReservationViewPageModule'
-  },
-  {
-    path: 'restaurant/:restaurantId/starter-course/:id',
-    loadChildren: './course/starter-course-view/starter-course-view.module#StarterCourseViewPageModule'
-  },
-  {
-    path: 'restaurant/:restaurantId/starter-courses',
-    loadChildren: './course/starter-course-list/starter-course-list.module#StarterCourseListPageModule'
-  },
-  {
-    path: 'restaurant/:restaurantId/starter-course-form',
-    loadChildren: './course/starter-course-form/starter-course-form.module#StarterCourseFormPageModule'
-  },
-  {
-    path: 'restaurant/:restaurantId/main-course/:id',
-    loadChildren: './course/main-course-view/main-course-view.module#MainCourseViewPageModule'
-  },
-  {
-    path: 'restaurant/:restaurantId/main-courses',
-    loadChildren: './course/main-course-list/main-course-list.module#MainCourseListPageModule'
-  },
-  {
-    path: 'restaurant/:restaurantId/main-course-form',
-    loadChildren: './course/main-course-form/main-course-form.module#MainCourseFormPageModule'
-  },
-  {
-    path: 'my-restaurant',
-    loadChildren: './restaurants/my-restaurant/my-restaurant.module#MyRestaurantPageModule'
-  },
-  {
-    path: 'employee/:restaurantId',
-    loadChildren: './restaurants/employee/employee.module#EmployeePageModule'
-  },
-  { path: 'restaurant/:restaurantId/dessert/:id', loadChildren: './course/dessert-view/dessert-view.module#DessertViewPageModule' },
-  { path: 'restaurant/:restaurantId/dessert-form', loadChildren: './course/dessert-form/dessert-form.module#DessertFormPageModule' },
-  { path: 'restaurant/:restaurantId/desserts', loadChildren: './course/dessert-list/dessert-list.module#DessertListPageModule' }
+	{
+		path: '',
+		redirectTo: 'sign-in',
+		pathMatch: 'full'
+	},
+	{
+		path: 'list',
+		loadChildren: './list/list.module#ListPageModule'
+	},
+	{
+		path: 'sign-up',
+		loadChildren: './sign-up/sign-up.module#SignUpPageModule'
+	},
+	{
+		path: 'restaurants',
+		loadChildren: './restaurants/restaurant-list/restaurants.module#RestaurantsPageModule'
+	},
+	{
+		path: 'sign-in',
+		loadChildren: './sign-in/sign-in.module#SignInPageModule'
+	},
+	{
+		path: 'restaurant-form',
+		//canActivate: [AuthGuard],
+		loadChildren: './restaurants/restaurant-form/restaurant-form.module#RestaurantFormPageModule'
+	},
+	{
+		path: 'restaurant/:id',
+		loadChildren: './restaurants/restaurant-view/restaurant-view.module#RestaurantViewPageModule'
+	},
+	{
+		path: 'reservations-list',
+		loadChildren: './reservations/reservations-list/reservations-list.module#ReservationsListPageModule'
+	},
+	{
+		path: 'reservation/:restaurantId/:reservationId',
+		loadChildren: './reservations/reservation-view/reservation-view.module#ReservationViewPageModule'
+	},
+	{
+		path: 'restaurant/:restaurantId/starter-course/:id',
+		loadChildren: './course/starter-course-view/starter-course-view.module#StarterCourseViewPageModule'
+	},
+	{
+		path: 'restaurant/:restaurantId/starter-courses',
+		loadChildren: './course/starter-course-list/starter-course-list.module#StarterCourseListPageModule'
+	},
+	{
+		path: 'restaurant/:restaurantId/starter-course-form',
+		loadChildren: './course/starter-course-form/starter-course-form.module#StarterCourseFormPageModule'
+	},
+	{
+		path: 'restaurant/:restaurantId/main-course/:id',
+		loadChildren: './course/main-course-view/main-course-view.module#MainCourseViewPageModule'
+	},
+	{
+		path: 'restaurant/:restaurantId/main-courses',
+		loadChildren: './course/main-course-list/main-course-list.module#MainCourseListPageModule'
+	},
+	{
+		path: 'restaurant/:restaurantId/main-course-form',
+		loadChildren: './course/main-course-form/main-course-form.module#MainCourseFormPageModule'
+	},
+	{
+		path: 'my-restaurant',
+		loadChildren: './restaurants/my-restaurant/my-restaurant.module#MyRestaurantPageModule'
+	},
+	{
+		path: 'employee/:restaurantId',
+		loadChildren: './restaurants/employee/employee.module#EmployeePageModule'
+	},
+	{ path: 'restaurant/:restaurantId/dessert/:id', loadChildren: './course/dessert-view/dessert-view.module#DessertViewPageModule' },
+	{ path: 'restaurant/:restaurantId/dessert-form', loadChildren: './course/dessert-form/dessert-form.module#DessertFormPageModule' },
+	{ path: 'restaurant/:restaurantId/desserts', loadChildren: './course/dessert-list/dessert-list.module#DessertListPageModule' },
+	{ path: 'restaurant/:restaurantId/ratings', loadChildren: './ratings/restaurant-ratings/restaurant-ratings.module#RestaurantRatingsPageModule' }
 
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+	],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/ratings/restaurant-ratings/restaurant-ratings.page.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { OverlayService } from 'src/app/common/services/overlay.service';
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantsService } from '../../restaurants/restaurants.service';
+import { RatingsService } from '../ratings.service';
+import { Location } from '@angular/common';
+
+@Component({
+	selector: 'app-restaurant-ratings',
+	templateUrl: './restaurant-ratings.page.html',
+	styleUrls: ['./restaurant-ratings.page.scss'],
+})
+export class RestaurantRatingsPage {
+	private restaurantId = null;
+	private restaurant = null;
+	private ratingList = null;
+	private updateRating = false;
+	private rating = { value: 0 };
+	private globalAvg = 0;
+	private userRating = null;
+
+	constructor(
+		private overlayService: OverlayService,
+		private location: Location,
+		private restaurantService: RestaurantsService,
+		private ratingService: RatingsService,
+		private route: ActivatedRoute) { }
+
+	async ionViewWillEnter() {
+		let loading = await this.overlayService.loading();
+		this.restaurantId = this.route.snapshot.paramMap.get('restaurantId');
+		this.restaurantService.getRestautant(this.restaurantId)
+			.subscribe(restaurantResponse => {
+				this.restaurant = restaurantResponse;
+			});
+		this.ratingService.listRatingsByRestaurantId(this.restaurantId)
+			.subscribe(ratingResponse => {
+				this.ratingList = ratingResponse;
+				this.globalAvg = this.ratingService.avgRatings(this.ratingList);
+				console.log('HIIIIIII', this.globalAvg);
+			});
+		this.ratingService.getUserRatingByRestaurantId(this.restaurantId)
+			.subscribe(userRating => {
+				if (userRating) {
+					this.updateRating = true;
+				}
+				this.userRating = userRating;
+				this.rating.value = userRating['value'];
+				loading.dismiss();
+			});
+	}
+
+	async registerRating(rating: any) {
+		let loading = await this.overlayService.loading();
+		if (this.updateRating) {
+			this.ratingService
+				.updateRating(this.restaurantId, this.userRating.id, this.rating)
+				.subscribe(() => {
+					this.location.back();
+					loading.dismiss();
+				}
+				);
+		} else {
+			this.ratingService
+				.createRating(this.restaurantId, this.rating)
+				.subscribe(() => {
+					this.location.back();
+					loading.dismiss();
+				}
+				);
+		}
+	}
+}

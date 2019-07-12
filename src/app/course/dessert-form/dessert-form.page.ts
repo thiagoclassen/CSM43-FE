4b7e9cf217e5bb4e1f04baeb963eb014386a2afb
Thiago Classen <tgclassen.dev@gmail.com>
2019-07-12 00:20:54 -0300

    dones

--- src/app/course/dessert-form/dessert-form.page.ts
+++ src/app/course/dessert-form/dessert-form.page.ts
@@ -67,8 +67,8 @@ export class DessertFormPage implements OnInit {
 
 	takePicture() {
 		const options: CameraOptions = {
-			quality: 100,
-			destinationType: this.camera.DestinationType.FILE_URI,
+			quality: 20,
+			destinationType: this.camera.DestinationType.DATA_URL,
 			encodingType: this.camera.EncodingType.JPEG,
 			mediaType: this.camera.MediaType.PICTURE
 		}
